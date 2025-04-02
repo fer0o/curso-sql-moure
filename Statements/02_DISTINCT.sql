dc83b65a90e9b0eb66ec36c46936b6f84495e88f
Fernando Medellin <fernandomedellin@Mac-mini-de-Fernando.local>
2025-04-02 01:21:49 -0600

    where

--- Statements/02_DISTINCT.sql
+++ Statements/02_DISTINCT.sql
@@ -1,3 +1,4 @@
+-- el comando DISTINCT se utiliza en sql para eliminar los registros duplicados de una consulta, es decir, si se tiene una tabla con registros duplicados, el comando DISTINCT se encargará de devolver solo un registro de cada uno de los duplicados.
 -- comando para ejecutar una consulta que no devuelva registros duplicados
 SELECT DISTINCT * FROM users;
 -- comando para ejecutar una consulta que no devuelva registros duplicados de la columna name
---
+++ Statements/03_WHERE.sql
@@ -0,0 +1,7 @@
+-- El comando WHERE se usa para filtrar los registros que devuelve una consulta, especificando condiciones que deben cumplir las filas. Esto te permite extraer solo los datos relevantes para tu consulta.
+--Limitando los datos que queremos recuperar
+SELECT * FROM users WHERE age = 32;
+--where age = between 20 and 30
+SELECT * FROM users WHERE age BETWEEN 20 AND 30;
+--where age <=30 esto hace que se devuelvan todos los registros de la tabla users donde la edad sea menor o igual a 30
+SELECT * FROM users WHERE age <= 30;
